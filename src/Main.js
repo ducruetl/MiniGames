77851d05badca1dfcdab9d4b22ae47e72ced24a3
ducruetl <ducruet.leo1@gmail.com>
2024-11-16 03:26:51 +0100

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+function Square() {
+  return (
+    <button className='square'>
+
+    </button>
+  )
+}
+
+function Main() {
+  return (
+    <>
+      <div className='header'>
+        <h1>Mini Games</h1>
+        <hr/>
+      </div>
+      <div className="main">
+      
+      </div>
+    </>
+  );
+}
+
+export default Main;
