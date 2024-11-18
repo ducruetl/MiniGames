8d40a840b3b2cca9e1b24a3f7d7d904ba43cc150
ducruetl <leo.ducruet@etu.univ-grenoble-alpes.fr>
2024-11-18 09:23:06 +0100

    Added react route and error-page

---
+++ src/error-page.jsx
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+
+import './style.css';
+
+export default function ErrorPage() {
+	const error = useRouteError();
+  console.error(error);
+
+	return (
+		<div id="error-page">
+			<h1>Oops!</h1>
+			<p>An unexpected error has occurred</p>
+			<p>
+        <i>{(error.status + " " + error.statusText) || error.status || error.statusText || error.message}</i>
+      </p>
+		</div>
+	);
+}
---
+++ src/routes/root.jsx
@@ -0,0 +1,16 @@
+import '../style.css';
+
+export default function Root() {
+	return (
+    <>
+      <div className='header'>
+        <h1>Mini Games</h1>
+        <hr/>
+      </div>
+      <div className='main'>
+       	 
+      </div>
+    </>
+  );
+
+}
